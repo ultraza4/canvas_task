68a3bc2fc91ed7a3a631b1451cd1839236e04564
azamat work <azamat@work>
2023-02-13 17:37:22 +0600

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="block">
+        <div class="block-item type">Text Message</div>
+        <div class="block-item text">Hi</div>
+        <div class="block-item step">next step</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+@Options({
+    data() {
+        return {
+            isDragging: false as boolean,
+
+        }
+    },
+    components: {
+
+    },
+    methods: {
+        handleDragStart() {
+            this.isDragging = true
+        },
+        handleDragEnd() {
+            
+        }
+    }
+})
+export default class Block extends Vue { }
+
+</script>
+
+<style lang="scss" scoped>
+.block {
+    width: 250px;
+    height: 200px;
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    border: 2px solid grey;
+
+    .block-item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .block-item+.block-item {
+        border-top: 1px solid grey;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="canvas">
+    <Block />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Block from '@/components/Block.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Block,
   },
 })
 export default class HomeView extends Vue {}
