7bff38acd627c6c1ea44412af915e1e0b68b0648
root <root@ip-10-0-1-89.us-west-1.compute.internal>
2023-09-11 08:49:57 +0000

    Second[Agent] javascript to typescript

---
+++ babel.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
+
+// Migrated code to TypeScript
+export default {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
--- vue.config.ts
+++ vue.config.ts
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+
+export default defineConfig({
   transpileDependencies: true
 })
